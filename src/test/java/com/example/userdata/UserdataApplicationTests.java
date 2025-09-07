a1e99854eea1c788e8bb5ce9edcf58af339c410f
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-09-07 14:24:14 +0530

    added import

--- src/test/java/com/example/userdata/UserdataApplicationTests.java
+++ src/test/java/com/example/userdata/UserdataApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.userdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
