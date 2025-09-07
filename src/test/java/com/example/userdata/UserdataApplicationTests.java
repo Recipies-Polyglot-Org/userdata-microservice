6ae4a6c5749a6129576a1558aca9d778eb3872c4
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-09-07 11:14:35 +0530

    added ci checks

--- src/test/java/com/example/userdata/UserdataApplicationTests.java
+++ src/test/java/com/example/userdata/UserdataApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.userdata;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserdataApplicationTests {
 
 	@Test
