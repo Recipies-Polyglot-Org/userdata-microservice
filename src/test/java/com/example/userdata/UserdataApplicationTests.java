ac9fd6acf48aaf5c0a8c747c0e3164d4f32c2935
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-09-07 14:20:19 +0530

    added h2

--- src/test/java/com/example/userdata/UserdataApplicationTests.java
+++ src/test/java/com/example/userdata/UserdataApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.userdata;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserdataApplicationTests {
 
